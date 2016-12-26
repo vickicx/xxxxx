146f4730ee5688f7c577c7269426853a62fc6151
cx <826987572@qq.com>
2016-12-26 14:16:51 +0800

    对啊

--- mingjieloan/Log&Res/Controller/RegisterViewController.m
+++ mingjieloan/Log&Res/Controller/RegisterViewController.m
@@ -142,46 +142,46 @@
 }
 
 - (void)sendYZMButton:(UIButton *)button {
-    
-    if (self.nameField.text.length == 11) {
-        [self getCode];
-        
-        __block int timeOut = 60;
-        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-        dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
-        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
-        dispatch_source_set_event_handler(_timer, ^{
-            if(timeOut<=0){ //倒计时结束，关闭
-                dispatch_source_cancel(_timer);
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    //设置界面的按钮显示 根据自己需求设置
-                    self.sendYZMButton.titleLabel.font = [UIFont systemFontOfSize:RealValue(14)];
-                    [self.sendYZMButton setTitle:@"获取验证码" forState:UIControlStateNormal];
-                    self.sendYZMButton.userInteractionEnabled = YES;
-                });
-            }else{
-                int seconds = timeOut % 61;
-                NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds];
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    //设置界面的按钮显示 根据自己需求设置
-                    //NSLog(@"____%@",strTime);
-                    [UIView beginAnimations:nil context:nil];
-                    [UIView setAnimationDuration:1];
-                    self.sendYZMButton.titleLabel.font = [UIFont systemFontOfSize:RealValue(12)];
-                    [self.sendYZMButton setTitle:[NSString stringWithFormat:@"%@秒后发送",strTime] forState:UIControlStateNormal];
-                    [UIView commitAnimations];
-                    self.sendYZMButton.userInteractionEnabled = NO;
-                });
-                timeOut--;
-            }
-        });
-        dispatch_resume(_timer);
-    } else {
-        JGProgressHUD *hud = [[JGProgressHUD alloc] initWithStyle:0];
-        hud.textLabel.text = @"请输入正确的手机号";
-        [hud showInView:self.view];
-        [hud dismissAfterDelay:1.5];
-    }
+    [self getCode];
+//    if (self.nameField.text.length == 11) {
+//        [self getCode];
+//        
+//        __block int timeOut = 60;
+//        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//        dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
+//        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+//        dispatch_source_set_event_handler(_timer, ^{
+//            if(timeOut<=0){ //倒计时结束，关闭
+//                dispatch_source_cancel(_timer);
+//                dispatch_async(dispatch_get_main_queue(), ^{
+//                    //设置界面的按钮显示 根据自己需求设置
+//                    self.sendYZMButton.titleLabel.font = [UIFont systemFontOfSize:RealValue(14)];
+//                    [self.sendYZMButton setTitle:@"获取验证码" forState:UIControlStateNormal];
+//                    self.sendYZMButton.userInteractionEnabled = YES;
+//                });
+//            }else{
+//                int seconds = timeOut % 61;
+//                NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds];
+//                dispatch_async(dispatch_get_main_queue(), ^{
+//                    //设置界面的按钮显示 根据自己需求设置
+//                    //NSLog(@"____%@",strTime);
+//                    [UIView beginAnimations:nil context:nil];
+//                    [UIView setAnimationDuration:1];
+//                    self.sendYZMButton.titleLabel.font = [UIFont systemFontOfSize:RealValue(12)];
+//                    [self.sendYZMButton setTitle:[NSString stringWithFormat:@"%@秒后发送",strTime] forState:UIControlStateNormal];
+//                    [UIView commitAnimations];
+//                    self.sendYZMButton.userInteractionEnabled = NO;
+//                });
+//                timeOut--;
+//            }
+//        });
+//        dispatch_resume(_timer);
+//    } else {
+//        JGProgressHUD *hud = [[JGProgressHUD alloc] initWithStyle:JGProgressHUDStyleExtraLight];
+//        hud.textLabel.text = @"请输入正确的手机号";
+//        [hud showInView:self.view];
+//        [hud dismissAfterDelay:1.5];
+//    }
 }
     
 
